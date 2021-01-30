f87e19d6c736ef8b4a5c85545f462e0aa8f6b1a2
ghaed <mhaghaed@gmail.com>
2021-01-29 17:56:33 -0800

    Integrated with Cassandra Javastax client successfully with sample data

--- vehicle-tracker/src/com/vehicletracker/servlet/vehicleTrackerServlet.java
+++ vehicle-tracker/src/com/vehicletracker/servlet/vehicleTrackerServlet.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+
 /**
  * Servlet implementation class vehicleTrackerServlet
  */
@@ -29,6 +33,15 @@ public class vehicleTrackerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+		Session session = cluster.connect();
+		String vehicleID = "CA6AFL218";
+		String trackDate = "2014-05-14";
+		String queryString = "SELECT time, latitude, longitude FROM vehicle_tracker.location WHERE vehicle_id = '" + vehicleID + "' AND date= '" + trackDate + "'";
+		ResultSet result = session.execute(queryString);
+		
+		
 		PrintWriter out = response.getWriter();
 		out.println("Hassan's Vehicle Tracker");
 	}
